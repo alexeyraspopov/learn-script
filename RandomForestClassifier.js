10acb4cac6351a80ee65ef8a7fc334d850472487
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-12-26 16:00:27 -0500

    create an interface of RandomForestClassifier

---
+++ RandomForestClassifier.js
@@ -0,0 +1,15 @@
+import DecisionTreeClassifier from './DecisionTreeClassifier';
+
+export default class RandomForestClassifier {
+  constructor(options) {
+
+  }
+
+  fit(data, labels) {
+
+  }
+
+  predict(data) {
+
+  }
+}

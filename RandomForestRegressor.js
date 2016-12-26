c1fe28a5f59f0da72feea1fb66a57a1e6e6c91a8
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-12-26 16:00:41 -0500

    create an interface of RandomForestRegressor

---
+++ RandomForestRegressor.js
@@ -0,0 +1,15 @@
+import DecisionTreeRegressor from './DecisionTreeRegressor';
+
+export default class RandomForestRegressor {
+  constructor(options) {
+
+  }
+
+  fit(data, labels) {
+
+  }
+
+  predict(data) {
+
+  }
+}

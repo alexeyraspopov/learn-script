a1b99f67721b347f0544717240330e793ba8b67c
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-12-26 16:21:22 -0500

    add RandomForest to the bundle

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import DecisionTreeClassifier from './DecisionTreeClassifier';
 import DecisionTreeRegressor from './DecisionTreeRegressor';
+import RandomForestClassifier from './RandomForestClassifier';
+import RandomForestRegressor from './RandomForestRegressor';
 
-export { DecisionTreeClassifier, DecisionTreeRegressor };
+export { DecisionTreeClassifier, DecisionTreeRegressor, RandomForestClassifier, RandomForestRegressor };
